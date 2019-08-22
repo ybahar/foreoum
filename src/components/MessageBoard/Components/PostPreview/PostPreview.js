ac07fa85364dcb4e2511ae7d859b8da3226da7fb
Yarin Bahar <yarinb1@gmail.com>
2019-08-22 16:15:07 +0300

    post preview changes

--- src/components/MessageBoard/Components/PostPreview/PostPreview.js
+++ src/components/MessageBoard/Components/PostPreview/PostPreview.js
@@ -7,14 +7,15 @@ export default class PostPreview extends Component{
     }
     render(){
         return (
-            <section className="post-preview-container">
-           <h3 className="post-preview-title">{this.props.post.title}</h3>
-           <section className="rating">
+    <section className="post-preview-container">
+        <h3 className="post-preview-title">{this.props.post.title}</h3>
+        <section className="rating">
              <button className="vote">Up</button>
+             <p>rating</p>
              <button className="vote">Down</button>
-           </section>
-           <p className="posted-at">{moment().startOf(this.props.post.createdAt).fromNow()}    </p>
-            </section>
+        </section>
+           <p className="posted-at">{moment().startOf(this.props.post.createdAt).fromNow()}</p>
+    </section>
         
         )
     }
